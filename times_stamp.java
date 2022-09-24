17210763872de6327b042cb2ecc1db7eefb0399d
Kosei Tajima <109095504+kou1133@users.noreply.github.com>
2022-09-25 04:19:41 +0900

    Add files via upload

---
+++ times_stamp.java
@@ -0,0 +1,14 @@
+
+class times_stamp
+{
+    public static void main(String args[]){
+        int left = 0;
+        int right = 0;
+    
+        for (left = 1; left < 10; left++){
+            for (right = 1; right < 10; right++){
+                System.out.println(left + "×" + right + "=" + left*right);
+            }
+        }
+    }
+}
